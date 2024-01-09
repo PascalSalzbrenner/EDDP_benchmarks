f36ed5495b8913274fc738aac4c0389b17650bae
Pascal Salzbrenner <pts28@cam.ac.uk>
2024-01-09 12:53:02 +0000

    New version of bench.sh which uses getopts to parse input and allows for specifying a custom ramble executable

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ A set of benchmarks to test the performance of ephemeral data-derived potentials
 
 Requires the EDDP software suite - at least `ramble`, which is part of the `repose` package - to be installed. It can be obtained under a GPL-2 license on the [Materials Theory Group website](https://www.mtg.msm.cam.ac.uk/Codes/EDDP).
 
-usage: `./bench.sh <list of number of cores>`
+Usage: `./bench.sh -c cores -e executable`
+
+Type `./bench.sh -h` for information on the input variables
 
 Currently contained benchmarks:
 - C:
--- bench.sh
+++ bench.sh
@@ -1,5 +1,37 @@
-if [ $# -eq 0 ];
-    then echo "usage: ./bench.sh <list of number of cores>"
+#!/bin/bash
+
+while getopts ":c:e:h" opt; do
+        case $opt in
+                c)
+                        # list of the core numbers which will be used for the benchmark runs 
+                        cores=("$OPTARG")
+                        until [[ $(eval "echo \${$OPTIND}") =~ ^-.* ]] || [ -z $(eval "echo \${$OPTIND}") ]; do
+                                cores+=($(eval "echo \${$OPTIND}"))
+                                OPTIND=$((OPTIND + 1))
+                        done;;
+                e)
+                        # name of the ramble executable used to run the job
+                        executable=$OPTARG;;
+                h)
+                        echo "Script to run the EDDP benchmarks contained in .tar.gz tarballs in this directory"
+                        echo "Usage: bench.sh [-c cores] [-e executable]"
+                        echo ""
+                        echo "Optional arguments"
+                        echo "-c cores               : list of core numbers at which the benchmarks will be run"
+                        echo "-e executable          : the ramble executable used to run the benchmarks"
+                        echo "-h                     : print this message and exit"
+                        echo ""
+                        echo "The following defaults apply: -c 1 $(nproc --all) -e ramble"
+                        exit 1
+        esac
+done
+
+if [ -z $cores ]; then
+        cores=(1 `nproc --all`)
+fi
+
+if [ -z $executable ]; then
+        executable="ramble"
 fi
 
 for i in *.tar.gz; do
@@ -11,9 +43,9 @@ for i in *.tar.gz; do
 
 	cd $root
 
-	for cores in "$@"; do
-		echo $cores "core(s)"
-		./run.sh $cores
+	for num_cores in ${cores[@]}; do
+		echo $num_cores "core(s)"
+		./run.sh $num_cores $executable
 	done
 
 	echo ""
